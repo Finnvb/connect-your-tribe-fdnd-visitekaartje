5f9251f244e888cfd88e4cdcfa1095d66894128d
Finnvb <finnvb@gmail.com>
2022-02-11 14:00:19 +0100

    FetchNameFomAPI function added

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// -------------------- Flip card --------------------
 const card = document.getElementsByClassName("container");
 
 card[0].addEventListener('click', flip);
@@ -15,6 +16,37 @@ function flip(event){
     }
   }
 };
+// -------------------- Flip card --------------------
 
 
 
+
+
+// -------------------- API handling --------------------
+const baseURL = "https://tribe.api.fdnd.nl/v1";
+const studentID = 13;
+const nameElement = document.getElementById("name");
+const githubHandle = document.getElementById("githubHandle");
+
+
+function fetchNameFromAPI(){
+  fetch(`${baseURL}/member`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((json) => {
+    let members = json.data; // array object met alle members 
+    members.forEach((member)=>{
+      if(member.memberId == 21){
+       
+       const nameAPI = member.name + " " + member.prefix + member.surname;
+       nameElement.innerHTML = nameAPI;
+      
+      }
+    })
+console.log(members);
+
+  })
+}
+  fetchNameFromAPI();
+  
