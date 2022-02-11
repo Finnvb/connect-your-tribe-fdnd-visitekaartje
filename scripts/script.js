6ee00e1befb3a49c0967b39ed13d1f9b94d89bd7
Finnvb <finnvb@gmail.com>
2022-02-11 19:01:52 +0100

    Patch

--- scripts/script.js
+++ scripts/script.js
@@ -26,6 +26,7 @@ const baseURL = "https://tribe.api.fdnd.nl/v1";
 const studentID = 13;
 const nameElement = document.getElementById("name");
 const githubHandleElement = document.getElementById("githubHandle");
+const bioElement = document.getElementById("bio");
 
 
 function fetchNameFromAPI() {
@@ -38,11 +39,14 @@ function fetchNameFromAPI() {
       members.forEach((member) => {
         if (member.memberId == 21) {
 
-          const nameAPI = member.name + " " + member.prefix + member.surname;
-          nameElement.innerHTML = nameAPI;
+          const nameFromAPI = member.name + member.prefix + member.surname;
+          nameElement.innerHTML = nameFromAPI;
 
-          const githubHandleAPI = member.githubHandle;
-           githubHandleElement.innerHTML = githubHandleAPI;
+          const githubHandleFromAPI = member.githubHandle;
+          githubHandleElement.innerHTML = githubHandleFromAPI;
+
+          const bioFromAPI = member.bio;
+          bioElement.innerHTML = bioFromAPI;
 
 
         }
@@ -51,4 +55,43 @@ function fetchNameFromAPI() {
 
     })
 }
+
 fetchNameFromAPI();
+
+
+
+
+function postAndRenderData() {
+  // 1. all bindings in the local scope for this function
+  const url = `${baseURL}/member`
+  const options = {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      memberId: 21,
+      squadId: 1,
+      type: "student",
+      nickname: "",
+      name: "Finn",
+      prefix: " van ",
+      surname: "Bekkum",
+      avatar: "https://avatars.githubusercontent.com/u/26089533?s=400&u=e586804b0a9f377d8f8198b00f5dccaca4c674f2&v=4",
+      githubHandle: "Finnvb",
+      bio: "Hallo, ik ben Finn van Bekkum. Mijn hobbies zijn gamen, programmeren en voetbal.",
+      url: "\r"
+    })
+  }
+
+  // 2. The logic
+  fetch(url, options).then(response => response.json())
+    .then(data => {
+      console.log(data)
+    }).catch(err => {
+      console.log(err)
+    })
+}
+
+postAndRenderData();
+// -------------------- API handling --------------------
