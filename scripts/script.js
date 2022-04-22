010b1f12cb039f19b2ac4cc71f5eac8b3cdbd471
Finnvb <finnvb@gmail.com>
2022-04-22 10:29:23 +0200

    added UI states

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,13 @@
-// -------------------- Flip card --------------------
-const card = document.getElementsByClassName("container");
+// -------------------- Flip card ----------------------
 
-card[0].addEventListener('click', flip);
+const card = document.querySelector(".container");
+
+card.addEventListener('click', flip);
 
 function flip(event) {
   const element = event.currentTarget;
-  if (element.className === "container") {
+  if (element.className === "showCard" || "container") {
+
 
     if (element.style.transform == "rotateY(180deg)") {
       element.style.transform = "rotateY(0deg)";
@@ -15,9 +17,25 @@ function flip(event) {
     }
   }
 };
-// -------------------- Flip card --------------------
+// -------------------- Flip card ----------------------
 
 
+
+// -------------------- preloader -------------------------
+const preloader = document.querySelector(".preloader");
+preloader.classList.toggle("preloaderEnabled");
+
+function hidePreloader() {
+  setTimeout(() => {
+    preloader.style.display = 'flex';
+    preloader.style.opacity = 0;
+    preloader.style.display = 'none';
+    card.classList.add('show');
+  }, 2000)
+}
+
+hidePreloader();
+// -------------------- preloader -------------------------
 
 
 
@@ -57,12 +75,12 @@ function fetchNameFromAPI() {
 }
 
 fetchNameFromAPI();
-
+postAndRenderData();
 
 
 
 function postAndRenderData() {
-  // 1. all bindings in the local scope for this function
+
   const url = `${baseURL}/member`
   const options = {
     method: 'PATCH',
@@ -79,19 +97,34 @@ function postAndRenderData() {
       surname: "Bekkum",
       avatar: "https://avatars.githubusercontent.com/u/26089533?s=400&u=e586804b0a9f377d8f8198b00f5dccaca4c674f2&v=4",
       githubHandle: "Finnvb",
-      bio: "Hallo, ik ben Finn van Bekkum. Mijn hobbies zijn gamen, programmeren en voetbal.",
+      bio: "Hallo, ik ben Finn van Bekkum. Ik ben eerstejaarsstudent op de FDND opleiding.",
       url: "\r"
     })
   }
 
-  // 2. The logic
+
   fetch(url, options).then(response => response.json())
     .then(data => {
       console.log(data)
     }).catch(err => {
       console.log(err)
+      errorHandling();
+      errorText.innerText = err;
     })
 }
 
-postAndRenderData();
 // -------------------- API handling --------------------
+
+
+// -------------------- error handling --------------------
+const error = document.querySelector(".error");
+const errorText = document.querySelector("#errorText");
+function errorHandling(){
+
+ card.style.display = 'none';
+ error.classList.remove('error')
+error.classList.add('show');
+preloader.style.display = 'none';
+ 
+}
+// -------------------- error handling --------------------
